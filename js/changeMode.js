9360cef559dd4976758451050655fef8ec43d81c
jordan-dez <kayajordan21.gmail.com>
2021-03-10 04:06:43 +0100

    touches entrée et espace sont foctionnelles

--- js/changeMode.js
+++ js/changeMode.js
@@ -1,9 +1,12 @@
 const switchToggle = document.querySelector('input[type="checkbox"');
 const myKbds = document.querySelectorAll('kbd') ;
+const toupcase = document.querySelectorAll('.maj');
 const toggleIcon = document.getElementById('toggle-icon');
 const capsIcon = document.getElementById('capskey');
+const toucheentree = document.getElementById('enterKey');
 var element = document.body;
 let myscreen = document.getElementById('myscreen');
+const toucheespace = document.getElementById('spacekey');
 
 
 function modetheme(event){
@@ -55,32 +58,41 @@ window.addEventListener("DOMContentLoaded", (event) => {
   });
   /****touche verrouillage */
   const capskey = document.getElementById("verrou");
+/*   capskey.onclick=function (){
+    capskey.checked? capslockActivated():capslockDisable();
+
+  } */
   capskey.addEventListener('change',verroMode);
   function verroMode(event){
       if(event.target.checked){
         capsIcon.children[0].classList.replace('fa-unlock', 'fa-lock');
         capslockActivated();
+        console.log('test');
       }else{
         capsIcon.children[0].classList.replace('fa-lock', 'fa-unlock');
         capslockDisable();
-        console.log(myKbds);
+        console.log('test ok');
+        //console.log(myKbds);
       }
   }
 
   function capslockActivated(){
-    for(let i=0;i<myKbds.length;i++){
+/*     for(let i=0;i<myKbds.length;i++){
         document.getElementsByTagName("kbd")[i].style.textTransform = "uppercase";
+        console.log(toupcase);
+       } */
+       toupcase.forEach(function(el,index){
+        el.innerText=el.innerText.toUpperCase();
+       });
        
-       }
   }
   function capslockDisable(){
-    for(let i=0;i<myKbds.length;i++){
-        document.getElementsByTagName("kbd")[i].style.textTransform = "lowercase";
-       
-       }
+    toupcase.forEach(function(el,index){
+      el.innerText=el.innerText.toLowerCase();
+     });
   }
     /*********les touches du clavier************ */
-    var touches = document.querySelectorAll('.keyboardRow kbd');
+    var touches = document.querySelectorAll('.keyboardRow .centerkey');
     //var touches = document.querySelectorAll('.centerkey');
     touches.forEach(function(el) {
   el.addEventListener('click', onClick, false);
@@ -90,6 +102,18 @@ function onClick(e) {
   var kbd = e.currentTarget;
   myscreen.value = myscreen.value + kbd.innerText;
   console.log(myscreen.value);
+ // console.log(toucheentree);
   //  myscreen.textContent = myscreen.innerText + kbd.innerText;
   //  console.log(kbd.innerText);
 }
+/************touche entrée */
+toucheentree.addEventListener('click',function(e){
+  myscreen.value +="\n";
+  console.log(myscreen);
+
+});
+/***touche entrée */
+toucheespace.addEventListener('click',(e)=>{
+  myscreen.value +=" ";
+});
+//&nbsp;
